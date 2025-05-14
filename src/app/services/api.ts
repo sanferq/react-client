c1815d133c447fa97ab6e84482ed445e923fe6dd
sanferq <proselkinmaksim@gmail.com>
2025-05-14 06:08:48 +0300

    ng-con-ap

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,26 +1,27 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
-import { BASE_URL } from "../../constants"
-import { RootState } from "../store"
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
+import { VITE_API_URL } from "../../constants"; // Используем переменную из constants
+import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${BASE_URL}/api`,
+  baseUrl: `${VITE_API_URL}/api`, 
   prepareHeaders: (headers, { getState }) => {
     const token =
-      (getState() as RootState).user.token || localStorage.getItem("token")
+      (getState() as RootState).user.token || localStorage.getItem("token");
 
     if (token) {
-      headers.set("authorization", `Bearer ${token}`)
+      headers.set("authorization", `Bearer ${token}`);
     }
 
-    return headers
+    return headers;
   },
-})
+});
 
-const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 })
+// Настроим повторные попытки для запросов
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 });
 
 export const api = createApi({
   reducerPath: "splitApi",
   baseQuery: baseQueryWithRetry,
   refetchOnMountOrArgChange: true,
   endpoints: () => ({}),
-})
+});
